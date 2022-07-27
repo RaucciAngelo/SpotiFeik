33dd8d278cc20ae201784730a17ea90d7230eb21
viksn0w <vittoriopicone30@gmail.com>
2022-07-27 11:32:24 +0200

    Artist fixed

--- SpotiFeik/src/main/java/com/example/demo/model/Artist.java
+++ SpotiFeik/src/main/java/com/example/demo/model/Artist.java
@@ -1,5 +1,117 @@
 package com.example.demo.model;
 
-public class Artist {
+ 
 
+import java.util.List;
+
+ 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+ 
+
+@Entity
+public class Artist {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long    id;
+    private String  name;
+    private String  birthdate;
+    private int     viewers;
+    private boolean verified;
+    @ManyToMany
+    @JoinColumn (name = "track")
+    private List <Track> track;
+    
+    
+    public Artist() {
+        super();
+    }
+    
+    public Artist(String name, String birthdate, int viewers, boolean verified, List <Track> track) {
+        super();
+        this.name = name;
+        this.birthdate = birthdate;
+        this.viewers = viewers;
+        this.verified = verified;
+        this.track = track;
+    }
+
+ 
+
+    public Long getId() {
+        return id;
+    }
+
+ 
+
+    public void setId_artista(Long id) {
+        this.id = id;
+    }
+
+ 
+
+    public String getName() {
+        return name;
+    }
+
+ 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+ 
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+ 
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+ 
+
+    public int getViewers() {
+        return viewers;
+    }
+
+ 
+
+    public void setViewers(int viewers) {
+        this.viewers = viewers;
+    }
+
+ 
+
+    public boolean getVerified() {
+        return verified;
+    }
+
+ 
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+ 
+
+    public List<Track> getTrack() {
+        return track;
+    }
+
+ 
+
+    public void setTrack(List<Track> track) {
+        this.track = track;
+    }
 }
--- SpotiFeik/src/main/java/com/example/demo/service/ArtistService.java
+++ SpotiFeik/src/main/java/com/example/demo/service/ArtistService.java
@@ -1,5 +1,45 @@
 package com.example.demo.service;
 
-public class ArtistService {
+ 
 
-}
+import java.util.List;
+
+ 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+ 
+
+import com.example.demo.model.Artist;
+import com.example.demo.repository.ArtistRepo;
+
+ 
+
+@Service
+public class ArtistService {
+    
+     @Autowired
+        private ArtistRepo artistRepo;
+        public List<Artist> getAllArtists() {
+            return artistRepo.findAll();
+        }
+        public void saveArtist(Artist artist, Long idArtist) {
+            artist = new Artist(artist.getName(), artist.getBirthdate(), artist.getViewers(), artist.getVerified(), artist.getTrack());
+            artistRepo.save(artist);
+        }
+        public void deleteArtist(Long id) {
+            artistRepo.deleteById(id);
+        }
+        @SuppressWarnings("deprecation")
+        public void updateArtist(Long id, Artist artist) {
+            Artist newArtist = new Artist();
+            newArtist = artistRepo.getById(id);
+            newArtist.setId_artista(artist.getId());
+            newArtist.setName(artist.getName());
+            newArtist.setBirthdate(artist.getBirthdate());
+            newArtist.setVerified(artist.getVerified());
+            newArtist.setTrack(artist.getTrack());
+            artistRepo.save(newArtist);
+        }
+    }
