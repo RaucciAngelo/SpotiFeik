f9b91368006a83a47cf66cd3a47c18486b0e300f
Luigi De Marco <ggdm1379@gmail.com>
2022-07-27 11:01:09 +0200

    Aggiunta di Track

--- SpotiFeik/src/main/java/com/example/demo/model/Track.java
+++ SpotiFeik/src/main/java/com/example/demo/model/Track.java
@@ -1,5 +1,117 @@
 package com.example.demo.model;
 
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
 public class Track {
+
+	// attribute
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private int duration;
+	private int plays;
+	private boolean explicit;
+	private String releaseDate;
+
+	@OneToMany(mappedBy = "idtrack")
+	private List<TrackArtist> artists;
+
+	@OneToMany(mappedBy = "idtrack")
+	private List<TrackPlaylist> playlists;
+
+	@OneToOne(mappedBy = "idtrack")
+	private Lyrics lyrics;
+
+	// contructor
+	public Track() {
+		super();
+	}
+
+	public Track(String title, int duration, int plays, boolean explicit, String releaseDate, Lyrics lyrics) {
+		super();
+		this.title = title;
+		this.duration = duration;
+		this.plays = plays;
+		this.explicit = explicit;
+		this.releaseDate = releaseDate;
+		this.lyrics = lyrics;
+	}
+
+	// get e set
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getPlays() {
+		return plays;
+	}
+
+	public void setPlays(int plays) {
+		this.plays = plays;
+	}
+
+	public boolean isExplicit() {
+		return explicit;
+	}
+
+	public void setExplicit(boolean explicit) {
+		this.explicit = explicit;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public List<TrackArtist> getArtists() {
+		return artists;
+	}
+
+	public void setArtists(List<TrackArtist> artists) {
+		this.artists = artists;
+	}
+
+	public List<TrackPlaylist> getPlaylists() {
+		return playlists;
+	}
+
+	public void setPlaylists(List<TrackPlaylist> playlists) {
+		this.playlists = playlists;
+	}
+
+	public Lyrics getLyrics() {
+		return lyrics;
+	}
+
+	public void setLyrics(Lyrics lyrics) {
+		this.lyrics = lyrics;
+	}
 
 }
---
+++ SpotiFeik/src/main/java/com/example/demo/service/TrackService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.*;
+import com.example.demo.repository.*;
+
+@Service
+public class TrackService {
+
+	@Autowired
+	private TrackRepo trackRepo;
+
+	@Autowired
+	private LyricsRepo lyricsRepo;
+
+	public List<Track> getAllTracks() {
+		return trackRepo.findAll();
+	}
+
+	public void save(Track track, Long idLyrics) {
+		Track newTrack = new Track(track.getTitle(), track.getDuration(), track.getPlays(), track.isExplicit(),
+				track.getReleaseDate(), lyricsRepo.getById(idLyrics));
+		trackRepo.save(newTrack);
+	}
+
+	public void delete(Long id) {
+		trackRepo.deleteById(id);
+	}
+
+	public void update(Long idTrack, Track track, Long idLyrics) {
+		Track updateTrack = trackRepo.getReferenceById(idTrack);
+		updateTrack = new Track(track.getTitle(), track.getDuration(), track.getPlays(), track.isExplicit(),
+				track.getReleaseDate(), lyricsRepo.getById(idLyrics));
+
+		trackRepo.save(updateTrack);
+
+	}
+
+}
