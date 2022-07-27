62e75a3a88491d0242c54b0327ca9889848a96ef
IvanLucignano <ivan.lucignano93@gmail.com>
2022-07-27 11:22:10 +0200

    Aggiunta Annotation

--- SpotiFeik/src/main/java/com/example/demo/repository/PlaylistRepo.java
+++ SpotiFeik/src/main/java/com/example/demo/repository/PlaylistRepo.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Playlist;
 
+@Repository
 public interface PlaylistRepo extends JpaRepository<Playlist, Long>{
 
 }
